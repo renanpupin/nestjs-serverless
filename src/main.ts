af32f4b17b8b613cb359d2df6a2cf056ebbbc9ea
Renan Pupin <renan.pupin@gmail.com>
2023-06-05 16:17:00 -0300

    serverless tets

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Option 1 - to initialize nestjs directly
   await app.listen(3000);
+
+  //Option 2 - to initialize nestjs with lambda
+  // await app.init();
+  // const expressApp = app.getHttpAdapter().getInstance();
+  // return serverlessExpress({ app: expressApp });
 }
+//Option 1 - to initialize nestjs directly
 bootstrap();
+
+//Option 2 - to initialize nestjs with lambda
+// export const handler: Handler = async (
+//   event: any,
+//   context: Context,
+//   callback: Callback,
+// ) => {
+//   server = server ?? (await bootstrap());
+//   return server(event, context, callback);
+// };
